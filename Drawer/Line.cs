b8afae21ae00beae043871e5bffdc13dd96f1be6
Obradovic Igor <extern.igor.obradovic@porsche.de>
2020-11-28 12:25:28 +0100

    Initial file parsing

---
+++ Drawer/Line.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawer
+{
+    public class Point
+    {
+        public Point(decimal xCoord, decimal yCoord)
+        {
+            m_xCoordinate = xCoord;
+            m_yCoordinate = yCoord;
+        }
+        public decimal CoordinateX
+        {
+            get { return m_xCoordinate; }
+        }
+        public decimal CoordinateY
+        {
+            get { return m_yCoordinate; }
+        }
+
+        private readonly decimal m_xCoordinate;
+        private readonly decimal m_yCoordinate;
+    }
+    
+    class Line
+    {
+        public Line(UInt64 lineNumber, UInt64 code, Point p)
+        {
+            m_LineNumber = lineNumber;
+            m_Coordinate = new Point(p.CoordinateX, p.CoordinateY);
+            m_Combinations = new List<UInt64>();
+            toCombinations(code);
+        }
+ 
+        private void toCombinations(UInt64 number)
+        {
+            while(number != 0)
+            {
+                m_Combinations.Add(number % 100);
+                number /= 100;
+            }
+        }
+        public List<UInt64> Combinations
+        {
+            get { return m_Combinations; }
+        }
+        public Point Coordinate
+        {
+            get { return m_Coordinate; }
+        }
+        private UInt64 m_LineNumber;
+        private Point m_Coordinate { get; set; }
+        private List<UInt64> m_Combinations { get; set; }
+    }
+}
---
+++ Drawer/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Drawer
+{
+    class Parser
+    {
+        private static readonly Parser singleInstance = new Parser();
+        public static Parser Instance { get {return singleInstance;} }
+        public List<Line> parse(string filePath)
+        {
+            string[] File = System.IO.File.ReadAllLines(filePath);
+            var lines = new List<Line>();
+            foreach (string line in File)
+            {
+                string[] numbers = line.Split(',');
+                UInt64 lineNumber = UInt64.Parse(numbers[0]);
+                decimal xCoordinate = decimal.Parse(numbers[1]);
+                decimal yCoordinate = decimal.Parse(numbers[2]);
+                decimal zCoordinate = decimal.Parse(numbers[3]);
+                UInt64 code = UInt64.Parse(numbers[4]);
+
+                lines.Add(new Line(lineNumber, code, new Point(xCoordinate, yCoordinate)));
+            }
+            return lines;
+        }
+    }
+}
